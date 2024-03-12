2741c1620be2d956c2e74f23e7ddcf91e0057ac8
luwoon <66871214+luwoon@users.noreply.github.com>
2024-03-12 15:22:51 +0800

    Update CS50 Practice.py

--- CS50 Practice.py
+++ CS50 Practice.py
@@ -58,7 +58,37 @@ class Jar:
         return self._capacity
 
     @property
+
+    # Add up prices of order
     def size(self):
         return self._size
 
 main()
+
+# Add up prices of order
+
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+total = 0
+
+while True:  # Infinite loop until break
+    try:
+        item = input("Item: ")
+        if not item:
+            break
+        elif item.title() in menu:
+            cost = menu[item.title()]
+            total += cost
+            print(f"Total: {total:.2f}")
+    except EOFError:
+        break
