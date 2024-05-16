3d2e8aa3e27e77bba308db13506e7049cbdd631b
luwoon <66871214+luwoon@users.noreply.github.com>
2024-05-16 14:46:58 +0800

    Update DataCamp: Analyzing Marketing Campaigns with pandas.py

--- DataCamp: Analyzing Marketing Campaigns with pandas.py
+++ DataCamp: Analyzing Marketing Campaigns with pandas.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 marketing = pd.read_csv('marketing.csv', parse_dates=['date_served', 'date_subscribed', 'date_canceled'])
 
@@ -68,3 +69,16 @@ daily_total = marketing.groupby(['date_served'])['user_id'].nunique()
 daily_subscribers = marketing[marketing['converted']==True].groupby(['date_served'])['user_id'].nunique()
 daily_conversion_rate = daily_subscribers/daily_total
 print(daily_conversion_rate)
+
+# reset index to turn results into DataFrame
+daily_conversion_rate = pd.DataFrame(daily_conversion_rates.reset_index())
+daily_conversion_rate.columns = ['date_served', 
+                              'conversion_rate']
+
+# create line chart using daily_conversion_rate
+daily_conversion_rate.plot('date_subscribed', 'conversion_rate')
+plt.title('Daily conversion rate\n', size = 16)
+plt.ylabel('Conversion rate (%)', size = 14)
+plt.xlabel('Date', size = 14)
+plt.ylim(0)
+plt.show()
