3fb41124bcaf4cb45d1d580832125e59f3265467
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-08 14:39:35 +0800

    Update Harvard CS50P.py

--- Harvard CS50P.py
+++ Harvard CS50P.py
@@ -15,3 +15,22 @@ def convert(s):
   return result
 
 main()
+
+
+# coin machine
+
+amt = 50
+valid = [25, 10, 5]
+
+while amt != 0:
+  print(f"Amount Due: {amt}")
+  inserted = int(input("Insert Coin: "))
+  if inserted not in valid:
+    continue
+  else:
+    amt -= inserted
+    if amt == 0:
+      print("Amount Due: 0")
+      break
+    else:
+      continue
