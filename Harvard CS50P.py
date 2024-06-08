d9f037093f05eab28c9581a2fdf682764908c6cb
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-08 15:26:23 +0800

    Update Harvard CS50P.py

--- Harvard CS50P.py
+++ Harvard CS50P.py
@@ -34,3 +34,23 @@ while amt != 0:
       break
     else:
       continue
+
+
+# omit vowels
+
+def main():
+  s = input("Input: ")
+  output = omit(s)
+  print(f"Output: {output}")
+
+def omit(s):
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  output = ""
+  for char in s:
+    if char in vowels:
+      output += ""
+    else:
+      output += char
+  return output  
+
+main()
