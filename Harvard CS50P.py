96653e25d44fed57767ffbfbd1c931e76d2e2954
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-08 21:56:24 +0800

    Update Harvard CS50P.py

--- Harvard CS50P.py
+++ Harvard CS50P.py
@@ -192,3 +192,33 @@ def omit(s):
   return output  
 
 main()
+
+
+# check validity of requested vanity license plate
+
+def main():
+  plate = input("Plate: ")
+  if is_valid(plate):
+    print("Valid")
+  else:
+    print("Invalid")
+
+
+def is_valid(s):
+  if 2 <= len(s) <= 6 and s.isalnum():
+    if s.isalpha(): 
+      return True
+    else:
+      if s[:2].isalpha() and s[-2:].isdigit():
+        for i in range(len(s)):
+          if s[i].isdigit():
+            if s[i].startswith("0") or s[i:].isalpha():
+              return False
+            else:
+              return True
+      else:
+        return False
+  else:
+    return False
+
+main()
