8fc584f6d7ff1dda35ea1276f04d644e217e882a
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-08 14:29:28 +0800

    Create Harvard CS50P.py

---
+++ Harvard CS50P.py
@@ -0,0 +1,17 @@
+# convert camel case to snake skin
+
+def main():
+  s = input("camelCase: ")
+  s = convert(s)
+  print(f"snake_skin: {s}")
+
+def convert(s):
+  result = ""
+  for index, a in enumerate(s):
+    if a.isupper() and index != 0:
+      result += '_' + a.lower()
+    else:
+      result += a.lower()
+  return result
+
+main()
