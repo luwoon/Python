9c1d72759dbb22cc153c5af919773b6abfb1a892
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-09 17:37:42 +0800

    Create Deep Thought.py

---
+++ CS50P/Problem Set 1/Deep Thought.py
@@ -0,0 +1,6 @@
+s = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip()
+
+if int(s) == 42 or s == "forty two" or s == "forty-two":
+  print("Yes")
+else:
+  print("No")
