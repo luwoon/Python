4cb547ec38c906b2365a9520fd12cd48504871ed
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-09 18:22:57 +0800

    Update Math Interpreter.py

--- CS50P/Problem Set 1/Math Interpreter.py
+++ CS50P/Problem Set 1/Math Interpreter.py
@@ -1,22 +1,22 @@
 def main():
-  s = input("Expression: ")
-  x, y, z = s.split(" ")
-  x = int(x)
-  z = int(z)
-  result = math(x, y, z)
-  print(f"{result:.1f}")
+    s = input("Expression: ")
+    x, y, z = s.split(" ")
+    x  = int(x)
+    z = int(z)
+    result = math(x, y, z)
+    print(f"{result:.1f}")
 
 
 def math(x, y, z):
-  if y == "+":
-    return x + z
-  elif y == "-":
-    return x - z
-  elif y == "/":
-    return float(x) / z
-  elif y == "*":
-    return x * z 
+    if y == "+":
+      return x + z
+    elif y == "-":
+      return x - z
+    elif y == "/":
+      return float(x) / z
+    elif y == "*":
+      return x * z 
 
 
 if __name__ == "__main__":
-  main()
+    main()
