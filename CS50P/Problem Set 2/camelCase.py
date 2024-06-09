1e488fd16210d62dd22105eb5572902a40db0543
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-09 17:39:27 +0800

    Create camelCase.py

---
+++ CS50P/Problem Set 2/camelCase.py
@@ -0,0 +1,16 @@
+def main():
+  s = input("camelCase: ")
+  s = convert(s)
+  print(f"snake_skin: {s}")
+
+def convert(s):
+  result = ""
+  for index, a in enumerate(s):
+    if a.isupper() and index != 0:
+      result += '_' + a.lower()
+    else:
+      result += a.lower()
+  return result
+
+if __name__ == "__main__":
+  main()
