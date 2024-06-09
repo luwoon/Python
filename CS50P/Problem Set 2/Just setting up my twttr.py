cb69092203afbcc6bec5b41e0e5068736b081a37
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-09 17:38:53 +0800

    Create Just setting up my twttr.py

---
+++ CS50P/Problem Set 2/Just setting up my twttr.py
@@ -0,0 +1,17 @@
+def main():
+  s = input("Input: ")
+  output = omit(s)
+  print(f"Output: {output}")
+
+def omit(s):
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  output = ""
+  for char in s:
+    if char in vowels:
+      output += ""
+    else:
+      output += char
+  return output  
+
+if __name__ == "__main__":
+  main()
