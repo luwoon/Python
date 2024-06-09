ea95b84270a135c2b4d601ee1bd30470d38f4d60
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-09 17:38:19 +0800

    Create Meal Time.py

---
+++ CS50P/Problem Set 1/Meal Time.py
@@ -0,0 +1,21 @@
+def main():
+    time = input("What time is it? ")
+    t = convert(time)
+    if 7.0 <= t <= 8.0:
+      print("breakfast time")
+    elif 12.0 <= t <= 13.0:
+      print("lunch time")
+    elif 18.0 <= t <= 19.0:
+      print("dinner time")
+    else:
+      pass
+
+def convert(time):
+    h, m = time.split(":")
+    h = int(h)
+    m = int(m)
+    t = h + m / 60.0
+    return t
+
+if __name__ == "__main__":
+    main()
