8cccaddc0a4a75a2a21a3fa7e4efbe0f3ef5e875
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-09 17:38:07 +0800

    Create Math Interpreter.py

---
+++ CS50P/Problem Set 1/Math Interpreter.py
@@ -0,0 +1,20 @@
+def main():
+  s = input("Expression: ")
+  x, y, z = s.split(" ")
+  x = int(x)
+  z = int(z)
+  result = math(x, y, z)
+  print(f"{result:.1f}")
+
+def math(x, y, z):
+  if y == "+":
+    return x + z
+  elif y == "-":
+    return x - z
+  elif y == "/":
+    return float(x) / z
+  elif y == "*":
+    return x * z 
+  
+if __name__ == "__main__":
+  main()
