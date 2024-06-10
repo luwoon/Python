2ff567c8cec4a33a4b2a97c7a4bb2c39cca88e79
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-10 11:58:49 +0800

    Create eeee.py

---
+++ CS50P/eeee.py
@@ -0,0 +1,27 @@
+# fuel
+
+def main():
+    get_frac()
+    if float(num) / den <= 0.01:
+        print("E")
+    elif float(num) / den >= 0.99:
+        print("F")
+    else:
+        output = num / den * 100
+        print(f"{output}%")
+
+def get_frac():
+    while True:
+        user_input = input("Fraction: ")
+        if "/" in user_input:
+            try:
+                num, den = user_input.split("/")
+                num = int(num)
+                den = int(den)
+                if den != 0 and num < den:
+                    return num, den
+            except ValueError:
+                pass
+
+if __name__ == "__main__":
+    main()
