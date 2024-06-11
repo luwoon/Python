ba02590f9a6df58fdf1572744b2895332c6ac0f0
luwoon <66871214+luwoon@users.noreply.github.com>
2024-06-11 14:51:55 +0800

    Create Frank, Ian and Glen’s Letters.py

---
+++ CS50P/Problem Set 4/Frank, Ian and Glen’s Letters.py
@@ -0,0 +1,18 @@
+from pyfiglet import Figlet
+import sys
+import random
+
+figlet = Figlet()
+font_list = figlet.getFonts()
+
+if len(sys.argv) == 1:
+    s = input("Input: ")
+    random_font = random.choice(font_list)
+    figlet.setFont(font=random_font)
+    print(figlet.renderText(s))
+elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font") and sys.argv[2] in font_list:
+    s = input("Input: ")
+    figlet.setFont(font = sys.argv[2])
+    print(figlet.renderText(s))
+else:
+    sys.exit("Error!")
