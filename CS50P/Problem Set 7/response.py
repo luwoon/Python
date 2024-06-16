8908564d244a1c64219a238c64a482b10298d388
luwoon <pangluwoon@gmail.com>
2024-06-16 18:18:37 +0800

    Created

---
+++ CS50P/Problem Set 7/response.py
@@ -0,0 +1,17 @@
+import validators
+
+
+def main():
+    email = input("What is your email address? ")
+    if validate_email(email):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def validate_email(email):
+    return validators.email(email)
+
+
+if __name__ == "__main__":
+    main()
